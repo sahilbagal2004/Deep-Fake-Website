857f4312138e209a2d512be27ab6ec2ec7954442
Sahil Satish Bagal <sahilbagal6445@gmail.com>
2026-03-21 20:44:41 +0530

    chore: setup deployment configurations

--- frontend_app/src/App.jsx
+++ frontend_app/src/App.jsx
@@ -33,7 +33,8 @@ function App() {
     formData.append("file", file)
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
+      const response = await fetch(`${API_URL}/predict`, {
         method: "POST",
         body: formData
       })
